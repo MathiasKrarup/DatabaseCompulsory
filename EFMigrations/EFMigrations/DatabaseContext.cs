9d31e1bd1d1f3f34580f0e46cfa15aa378de29e4
Matty <math249e@easv365.dk>
2024-03-08 13:31:24 +0100

    Defined initial DbContext and model and made the initial migration

--- EFMigrations/EFMigrations/DatabaseContext.cs
+++ EFMigrations/EFMigrations/DatabaseContext.cs
@@ -1,7 +1,16 @@
-﻿namespace EFMigrations
-{
-    public class DatabaseContext
-    {
+﻿using EFMigrations.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace EFMigrations
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=localhost,1435;Database=DatabaseSchemaMigration;User Id=sa;Password=SuperSecret7!;Trusted_Connection=False;TrustServerCertificate=True;");
+        }
     }
 }
