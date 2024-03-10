0579731780fef836264bbc45db190dbc721d4bca
AndreasBerthelsen <andr59j9@easv365.dk>
2024-03-10 21:13:03 +0100

    Added ProductRatings

---
+++ EFMigrations/EFMigrations/Models/ProductRating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFMigrations.Models
+{
+    // ProductRating model
+    public class ProductRating
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
